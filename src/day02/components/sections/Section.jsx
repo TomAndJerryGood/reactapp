c725dda0a0d8b4e84c59fe3602311e2c986e7fd0
TomAndJerryGood <jayungh@gmail.com>
2023-07-13 21:04:08 +0900

    react-app

---
+++ src/day02/components/sections/Section.jsx
@@ -0,0 +1,10 @@
+import './Section.css'
+
+const Section = ()=>{
+    return (
+        <section className='section-container'>
+            contents
+        </section>
+    )
+}
+export default Section;
